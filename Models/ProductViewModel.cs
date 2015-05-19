3fc2ffd586f43ce044e071f3db1cd5da057df495
cleophasmashiri <cleophasmashiri@gmail.com>
2015-05-19 08:50:44 +0200

    first commit

---
+++ Models/ProductViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MyNamespace
+{
+    public class ProductViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Properties/AppSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace weshop
+{
+    public class AppSettings
+    {
+        public string SiteTitle { get; set; }
+    }
+}
